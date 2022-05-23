bbc2cee9577cf0b4944646480ed37bbd6474ce21
Tupical <d.avakian95@gmail.com>
2022-05-23 21:55:17 +0400

    feat: show more

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function javascriptCoder(data) {
     const welcome = document.createElement("h1");
     
     header.className = "header";
-    mainImg.setAttribute("src", "/images/starting-pg.jpg");
+    mainImg.setAttribute("src", "./images/starting-pg.jpg");
     mainImg.setAttribute("alt", "Main bookshop picture");
     welcome.innerHTML = 'Welcome to the world of books!';
     header.appendChild(mainImg);
@@ -27,11 +27,13 @@ function javascriptCoder(data) {
     const columns = document.createElement("div");
     const columnTitle = document.createElement("div");
     const headingColumn1 = document.createElement("h2");
+    const overlay = document.createElement("div");
     
     mainContainer.className = "main-container";
     columns.className = "bookshop-columns";
     columnTitle.className = "column-title";
     headingColumn1.innerHTML = "Book Catalog";
+    overlay.setAttribute("id", "overlay");
 
     columnTitle.appendChild(headingColumn1);
     columns.appendChild(columnTitle);
@@ -50,7 +52,15 @@ function javascriptCoder(data) {
     const activeBtn = document.createElement("div");
     const moreInfo = document.createElement("button");
     const addToBag = document.createElement("button");
-    //const readmore = document.createElement("div");
+    const descriptionModal = document.createElement("div");
+    const descriptionModalHeader = document.createElement("header");
+    const descriptionModalHeaderTitle = document.createElement("div");
+    const descriptionModalHeaderTitleParagraph = document.createElement("p");
+    const descriptionModalHeaderButton = document.createElement("button");
+    const descriptionModalBody = document.createElement("div");
+    const descriptionModalBodyParagraph = document.createElement("p");
+    
+
 
 
     
@@ -67,9 +77,68 @@ function javascriptCoder(data) {
     informationh31.innerHTML = "Price: $" + data[i].price;
     activeBtn.className = "active-btn";
     moreInfo.className = "more-info";
+    moreInfo.setAttribute("data-modal-target", "#modal");
     moreInfo.innerHTML = "Show more";
     addToBag.className = "add-to-bag";
     addToBag.innerHTML = "Add to bag";
+
+    //modal
+    descriptionModal.className = "modal";
+    descriptionModal.setAttribute("id", "modal");
+    descriptionModalHeader.className = "modal-header";
+    descriptionModalHeaderTitle.className = "title";
+    descriptionModalHeaderButton.setAttribute("data-close-button", "");
+    descriptionModalHeaderButton.className = "close-button";
+    descriptionModalBody.className = "modal-body";
+    descriptionModalHeaderTitleParagraph.innerHTML = "Description";
+    descriptionModalBody.innerHTML = data[i].description;
+    descriptionModalHeaderButton.innerHTML = "&times;";
+    
+    
+    //append
+    descriptionModalHeaderTitle.appendChild(descriptionModalHeaderTitleParagraph);
+    descriptionModalHeader.appendChild(descriptionModalHeaderTitle);
+    descriptionModalHeader.appendChild(descriptionModalHeaderButton);
+    descriptionModal.appendChild(descriptionModalHeader);
+    descriptionModalBody.appendChild(descriptionModalBodyParagraph);
+    descriptionModal.appendChild(descriptionModalBody);
+
+
+    //function
+
+    const openModalButtons = document.querySelectorAll('[data-modal-target]')
+    const closeModalButtons = document.querySelectorAll('[data-close-button]')
+    
+
+    moreInfo.addEventListener('click', () => {
+            openModal(descriptionModal);
+        })
+    
+
+    overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal)
+    })
+    })
+
+    descriptionModalHeaderButton.addEventListener('click', () => {
+        closeModal(descriptionModal)
+    })
+    
+
+    function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+    }
+
+    function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+    }
+
 
     bookPicture.appendChild(bookImg);
     books.appendChild(bookPicture);
@@ -78,10 +147,14 @@ function javascriptCoder(data) {
     description.appendChild(informationh31);
     information.appendChild(description);
     activeBtn.appendChild(moreInfo);
+    activeBtn.appendChild(descriptionModal);
+    
     activeBtn.appendChild(addToBag);
     information.appendChild(activeBtn);
     books.appendChild(information);
     columns.appendChild(books);
+
+    
 
     //moreInfo.addEventListener("mouseenter", () => )
 
@@ -90,6 +163,7 @@ function javascriptCoder(data) {
     const columns2 = document.createElement("div");
     const columnTitle2 = document.createElement("div");
     const headingColumn2 = document.createElement("h2");
+   
 
     columns2.className = "bookshop-columns";
     columnTitle2.className = "column-title";
@@ -98,6 +172,7 @@ function javascriptCoder(data) {
     columnTitle2.appendChild(headingColumn2);
     columns2.appendChild(columnTitle2);
     mainContainer.appendChild(columns2);
+    mainContainer.appendChild(overlay);
 
 
 
@@ -107,14 +182,3 @@ function javascriptCoder(data) {
     fragment.append(mainContainer);
     document.body.append(fragment);
 }
-
-
-
-
-// connect json and transfer data;
-
-    
-
-
-
-
